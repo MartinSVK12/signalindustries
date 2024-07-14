458dad80730703811c528cf5f9af0e769846cfac
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2024-07-14 22:37:41 +0200

    Added fluids types to catalyst fluid registry.

--- src/main/java/sunsetsatellite/signalindustries/api/impl/catalyst/SignalIndustriesFluidPlugin.java
+++ src/main/java/sunsetsatellite/signalindustries/api/impl/catalyst/SignalIndustriesFluidPlugin.java
@@ -4,17 +4,23 @@ package sunsetsatellite.signalindustries.api.impl.catalyst;
 import org.slf4j.Logger;
 import sunsetsatellite.catalyst.CatalystFluids;
 import sunsetsatellite.catalyst.fluids.registry.FluidContainerRegistryEntry;
+import sunsetsatellite.catalyst.fluids.util.FluidType;
 import sunsetsatellite.signalindustries.SIBlocks;
 import sunsetsatellite.signalindustries.SIItems;
 import sunsetsatellite.signalindustries.SignalIndustries;
 
 import java.util.Collections;
 
+import static sunsetsatellite.catalyst.Catalyst.listOf;
+
 public class SignalIndustriesFluidPlugin {
     public void initializePlugin(Logger logger) {
         logger.info("Loading fluids from signalindustries..");
+        CatalystFluids.TYPES.register(new FluidType("signalindustries:energy",listOf(SIBlocks.energyFlowing,SIBlocks.energyStill)));
+        CatalystFluids.TYPES.register(new FluidType("signalindustries:burntSignalum",listOf(SIBlocks.burntSignalumFlowing,SIBlocks.burntSignalumStill)));
+        logger.info("Loading fluid containers from signalindustries..");
         FluidContainerRegistryEntry entry = new FluidContainerRegistryEntry(SignalIndustries.MOD_ID, SIItems.signalumCrystalBattery, SIItems.signalumCrystalEmpty, Collections.singletonList(SIBlocks.energyFlowing));
-        CatalystFluids.CONTAINERS.register(SignalIndustries.key("signalumEnergy"),entry);
+        CatalystFluids.CONTAINERS.register(SignalIndustries.key("signalumCrystal"),entry);
         entry = new FluidContainerRegistryEntry(SignalIndustries.MOD_ID, SIItems.signalumSaber, SIItems.signalumSaber,  Collections.singletonList(SIBlocks.energyFlowing));
         CatalystFluids.CONTAINERS.register(SignalIndustries.key("signalumSaber"),entry);
         entry = new FluidContainerRegistryEntry(SignalIndustries.MOD_ID, SIItems.basicSignalumDrill, SIItems.basicSignalumDrill,  Collections.singletonList(SIBlocks.energyFlowing));
