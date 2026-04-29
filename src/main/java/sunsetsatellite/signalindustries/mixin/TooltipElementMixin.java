005a3635d0eeaead90806473189214f4439df639
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-04-29 14:21:58 +0200

    Show slot id in tooltip when extra item info is enabled.

--- src/main/java/sunsetsatellite/signalindustries/mixin/TooltipElementMixin.java
+++ src/main/java/sunsetsatellite/signalindustries/mixin/TooltipElementMixin.java
@@ -1,6 +1,7 @@
 package sunsetsatellite.signalindustries.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.TooltipElement;
 import net.minecraft.client.gui.guidebook.SlotGuidebook;
 import net.minecraft.core.WeightedRandomBag;
@@ -11,6 +12,7 @@ import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.net.command.TextFormatting;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,6 +20,9 @@ import sunsetsatellite.signalindustries.recipes.entry.RecipeEntryMachineRandomOu
 
 @Mixin(value = TooltipElement.class, remap = false)
 public class TooltipElementMixin {
+
+    @Shadow
+    Minecraft mc;
 
     @Inject(method = "getTooltipText(Lnet/minecraft/core/item/ItemStack;ZLnet/minecraft/core/player/inventory/slot/Slot;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;append(Ljava/lang/String;)Ljava/lang/StringBuilder;", ordinal = 0, shift = At.Shift.AFTER))
     public void getTooltipText(ItemStack itemStack, boolean showDescription, Slot slot, CallbackInfoReturnable<String> cir, @Local StringBuilder text) {
@@ -36,6 +41,14 @@ public class TooltipElementMixin {
                     text.append('\n').append(TextFormatting.LIGHT_GRAY).append(String.format("%.2f", percent)).append("%");
                 }
             }
+        }
+    }
+
+    @Inject(method = "getTooltipText(Lnet/minecraft/core/item/ItemStack;ZLnet/minecraft/core/player/inventory/slot/Slot;)Ljava/lang/String;", at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;append(I)Ljava/lang/StringBuilder;", ordinal = 1, shift = At.Shift.AFTER))
+    public void getTooltipText2(ItemStack itemStack, boolean showDescription, Slot slot, CallbackInfoReturnable<String> cir, @Local StringBuilder text) {
+        boolean debug = this.mc.gameSettings.showItemDebugInfo.value;
+        if(debug && slot != null){
+            text.append("\n").append(TextFormatting.LIGHT_GRAY).append("Slot ID: ").append(slot.index);
         }
     }
 
