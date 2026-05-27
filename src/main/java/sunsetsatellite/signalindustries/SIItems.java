15edf6ec7b50f33e23981e1be3355b664af451e9
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-05-27 14:20:49 +0200

    Readded simple items.

--- src/main/java/sunsetsatellite/signalindustries/SIItems.java
+++ src/main/java/sunsetsatellite/signalindustries/SIItems.java
@@ -1,6 +1,7 @@
 package sunsetsatellite.signalindustries;
 
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.Items;
 import sunsetsatellite.catalyst.Catalyst;
 import sunsetsatellite.catalyst.core.util.DataInitializer;
 import sunsetsatellite.signalindustries.items.attachments.ItemAttachment;
@@ -167,6 +168,82 @@ public class SIItems extends DataInitializer {
 	public void init() {
 		if (initialized) return;
 		LOGGER.info("Initializing items...");
+
+		rawSignalumCrystal = simpleItem("rawSignalumCrystal", "raw_signalite_crystal", "rawSignalumCrystal", "raw_signalum_crystal");
+		signalumCrystal = simpleItem("signalumCrystal", "signalite_crystal", "signalumCrystal", "signalum_crystal");
+		signalumCrystalEmpty = simpleItem("signalumCrystalEmpty", "signalite_crystal_empty", "signalumCrystalEmpty", "signalum_crystal_empty");
+		awakenedSignalumCrystal = simpleItem("awakenedSignalumCrystal", "awakened_signalite_crystal", "awakenedSignalumCrystal", "awakened_signalum_crystal").setMaxStackSize(1);
+		awakenedSignalumFragment = simpleItem("awakenedSignalumFragment", "awakened_signalite_fragment", "awakenedSignalumFragment", "awakened_signalum_fragment");
+		coalDust = simpleItem("coalDust", "coal_dust", "coalDust", "coaldust");
+		ironDust = simpleItem("ironDust", "iron_dust", "ironDust", "iron_dust");
+		goldDust = simpleItem("goldDust", "gold_dust", "goldDust", "gold_dust");
+		netherCoalDust = simpleItem("netherCoalDust", "nether_coal_dust", "netherCoalDust", "nethercoaldust");
+		tinyNetherCoalDust = simpleItem("tinyNetherCoalDust", "tiny_nether_coal_dust", "tinyNetherCoalDust", "tiny_nether_coal_dust");
+		emptySignalumCrystalDust = simpleItem("emptySignalumCrystalDust", "empty_signalite_crystal_dust", "signalumCrystalDust", "empty_signalum_dust");
+		saturatedSignalumCrystalDust = simpleItem("saturatedSignalumCrystalDust", "saturated_signalite_crystal_dust", "saturatedSignalumCrystalDust", "saturated_signalum_dust");
+		awakenedSignalumCrystalDust = simpleItem("awakenedSignalumCrystalDust", "awakened_signalite_crystal_dust", "awakenedSignalumCrystalDust", "awakened_signalum_dust");
+		ironPlateHammer = simpleItem("ironPlateHammer", "iron_plate_hammer", "ironPlateHammer", "plate_hammer").setMaxStackSize(1);
+		ironPlateHammer.setContainerItem(ironPlateHammer);
+		cobblestonePlate = simpleItem("cobblestonePlate", "cobblestone_plate", "cobblestonePlate", "cobblestone_plate");
+		stonePlate = simpleItem("stonePlate", "stone_plate", "stonePlate", "stone_plate");
+		crystalAlloyPlate = simpleItem("crystalAlloyPlate", "crystal_alloy_plate", "crystalAlloyPlate", "crystal_alloy_plate");
+		steelPlate = simpleItem("steelPlate", "steel_plate", "steelPlate", "steel_plate");
+		reinforcedCrystalAlloyPlate = simpleItem("reinforcedCrystalAlloyPlate", "reinforced_crystal_alloy_plate", "reinforcedCrystalAlloyPlate", "reinforcedcrystalalloyplate");
+		saturatedSignalumAlloyPlate = simpleItem("saturatedSignalumAlloyPlate", "saturated_signalite_alloy_plate", "saturatedSignalumAlloyPlate", "saturated_signalum_alloy_plate");
+		dilithiumPlate = simpleItem("dilithiumPlate", "dilithium_plate", "dilithiumPlate", "dilithium_plate");
+		voidAlloyPlate = simpleItem("voidAlloyPlate", "void_alloy_plate", "voidAlloyPlate", "void_alloy_plate");
+		awakenedAlloyPlate = simpleItem("awakenedAlloyPlate", "awakened_alloy_plate", "awakenedAlloyPlate", "awakened_alloy_plate");
+		crystalAlloyIngot = simpleItem("crystalAlloyIngot", "crystal_alloy_ingot", "crystalAlloyIngot", "crystal_alloy");
+		reinforcedCrystalAlloyIngot = simpleItem("reinforcedCrystalAlloyIngot", "reinforced_crystal_alloy_ingot", "reinforcedCrystalAlloyIngot", "reinforced_crystal_alloy");
+		saturatedSignalumAlloyIngot = simpleItem("saturatedSignalumAlloyIngot", "saturated_signalite_alloy_ingot", "saturatedSignalumAlloyIngot", "saturated_signalum_alloy");
+		voidAlloyIngot = simpleItem("voidAlloyIngot", "void_alloy_ingot", "voidAlloyIngot", "void_alloy");
+		awakenedAlloyIngot = simpleItem("awakenedAlloyIngot", "awakened_alloy_ingot", "awakenedAlloyIngot", "awakened_alloy");
+		diamondCuttingGear = simpleItem("diamondCuttingGear", "diamond_cutting_gear", "diamondCuttingGear", "diamond_cutting_gear");
+		signalumCuttingGear = simpleItem("signalumCuttingGear", "signalite_cutting_gear", "signalumCuttingGear", "signalum_cutting_gear");
+		realityString = simpleItem("realityString", "string_of_reality", "realityString", "string_of_reality");
+		dilithiumShard = simpleItem("dilithiumShard", "dilithium_shard", "dilithiumShard", "dilithium_shard");
+		monsterShard = simpleItem("monsterShard", "monster_shard", "monsterShard", "monster_shard");
+		infernalFragment = simpleItem("infernalFragment", "infernal_fragment", "infernalFragment", "infernal_fragment");
+		evilEye = simpleItem("evilEye", "evil_eye", "evilEye", "evileye").setMaxStackSize(4);
+		infernalEye = simpleItem("infernalEye", "infernal_eye", "infernalEye", "infernaleye").setMaxStackSize(4);
+		dimensionalShard = simpleItem("dimensionalShard", "dimensional_shard", "dimensionalShard", "dimensional_shard");
+		crystalChip = simpleItem("crystalChip", "crystal_chip", "crystalChip", "crystal_chip");
+		pureCrystalChip = simpleItem("pureCrystalChip", "pure_crystal_chip", "pureCrystalChip", "pure_crystal_chip");
+		awakenedCrystalChip = simpleItem("awakenedCrystalChip", "awakened_crystal_chip", "awakenedCrystalChip", "awakened_crystal_chip");
+		basicEnergyCore = simpleItem("basicEnergyCore", "basic_energy_core", "basicEnergyCore", "basic_energy_core");
+		reinforcedEnergyCore = simpleItem("reinforcedEnergyCore", "reinforced_energy_core", "reinforcedEnergyCore", "reinforced_energy_core");
+		awakenedEnergyCore = simpleItem("awakenedEnergyCore", "awakened_energy_core", "awakenedEnergyCore", "awakened_energy_core");
+		basicDrillBit = simpleItem("basicDrillBit", "basic_drill_bit", "basicDrillBit", "basic_drill_bit");
+		reinforcedDrillBit = simpleItem("reinforcedDrillBit", "reinforced_drill_bit", "reinforcedDrillBit", "reinforced_drill_bit");
+		basicDrillCasing = simpleItem("basicDrillCasing", "basic_drill_casing", "basicDrillCasing", "basic_drill_casing");
+		reinforcedDrillCasing = simpleItem("reinforcedDrillCasing", "reinforced_drill_casing", "reinforcedDrillCasing", "reinforced_drill_casing");
+		pulsarShell = simpleItem("pulsarShell", "pulsar_shell", "pulsarShell", "pulsar_shell");
+		pulsarInnerCore = simpleItem("pulsarInnerCore", "pulsar_inner_core", "pulsarInnerCore", "pulsar_inner_core");
+		pulsarOuterCore = simpleItem("pulsarOuterCore", "pulsar_outer_core", "pulsarOuterCore", "pulsar_outer_core");
+		itemManipulationCircuit = simpleItem("itemManipulationCircuit", "item_manipulation_circuit", "itemManipulationCircuit", "item_manipulation_circuit");
+		fluidManipulationCircuit = simpleItem("fluidManipulationCircuit", "fluid_manipulation_circuit", "fluidManipulationCircuit", "fluid_manipulation_circuit");
+		dilithiumControlCore = simpleItem("dilithiumControlCore", "dilithium_control_core", "dilithiumControlCore", "dilithium_control_core");
+		warpManipulatorCircuit = simpleItem("warpManipulatorCircuit", "warp_manipulator_circuit", "warpManipulatorCircuit", "warp_manipulator_circuit");
+		dilithiumChip = simpleItem("dilithiumChip", "dilithium_chip", "dilithiumChip", "dilithium_chip");
+		dimensionalChip = simpleItem("dimensionalChip", "dimensional_chip", "dimensionalChip", "dimensional_chip");
+		attachmentPoint = simpleItem("attachmentPoint", "attachment_point", "attachmentPoint", "attachment_point");
+		blankAbilityModule = simpleItem("blankAbilityModule", "blank_ability_module", "blankAbilityModule", "blank_module");
+		abilityContainerCasing = simpleItem("abilityContainerCasing", "ability_container_casing", "abilityContainerCasing", "ability_container_casing");
+		blankChip = simpleItem("blankChip", "blank_chip", "romChip.blank", "blank_chip");
+		condensedMilkCan = simpleItem("condensedMilkCan", "condensed_milk_can", "condensedMilkCan", "condensed_milk_can").setMaxStackSize(1);
+		bucketCaramel = simpleItem("bucketCaramel", "bucket_caramel", "bucketCaramel", "bucket_caramel").setMaxStackSize(1).setContainerItem(Items.BUCKET_IRON);
+		caramelPlate = simpleItem("caramelPlate", "caramel_plate", "caramelPlate", "caramel_plate");
+		signalumAlloyMesh = simpleItem("signalumAlloyMesh", "signalum_alloy_mesh", "signalumAlloyMesh", "signalum_alloy_mesh");
+		crystalWingPart = simpleItem("crystalWingPart", "crystal_wing_part", "crystalWingPart", "crystal_wing_part").setMaxStackSize(6);
+		precisionControlChip = simpleItem("precisionControlChip", "precision_control_chip", "romChip.precision", "precision_control_chip");
+		clearKey = simpleItem("clearKey", "clear_key", "clearKey", "clear_key");
+		saturatedKey = simpleItem("saturatedKey", "saturated_key", "saturatedKey", "saturated_key");
+		unlimitedChip = simpleItem("unlimitedChip", "unlimited_chip", "romChip.unlimited", "unlimited_chip");
+		heatingCoil = simpleItem("heatingCoil", "heating_coil", "heatingCoil", "heating_coil");
+		coolingCoil = simpleItem("coolingCoil", "cooling_coil", "coolingCoil", "cooling_coil");
+		//ashenTreeSapling = simpleItem("ashenTreeSapling", "ashen_tree_sapling","sapling.ashen","ashen_tree_sapling");
+
+		raziel = simpleItem("raziel", "raziel", "raziel", "raziel");
 
 		infiniteSignalumCrystal = customItem(() ->
 				new ItemSignalumCrystal(
