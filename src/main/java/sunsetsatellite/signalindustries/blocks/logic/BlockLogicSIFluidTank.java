7de065ee39052f4f9a0071d048fdc625730e0917
MartinSVK12 <37455793+MartinSVK12@users.noreply.github.com>
2026-09-14 11:16:33 +0200

    Fluid tanks can now be interacted with by clicking on them with a bucket or any other fluid containing item.

--- src/main/java/sunsetsatellite/signalindustries/blocks/logic/BlockLogicSIFluidTank.java
+++ src/main/java/sunsetsatellite/signalindustries/blocks/logic/BlockLogicSIFluidTank.java
@@ -4,14 +4,20 @@ import com.mojang.nbt.tags.CompoundTag;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.player.Player;
 import net.minecraft.core.enums.EnumDropCause;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.util.HardIllegalArgumentException;
 import net.minecraft.core.util.collection.NamespaceID;
+import net.minecraft.core.util.helper.Side;
 import net.minecraft.core.world.World;
 import net.minecraft.core.world.pos.TilePosc;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jspecify.annotations.NonNull;
+import sunsetsatellite.catalyst.fluids.api.IItemFluidContainer;
 import sunsetsatellite.catalyst.fluids.util.Fluid;
+import sunsetsatellite.catalyst.fluids.util.FluidStack;
 import sunsetsatellite.signalindustries.blocks.logic.base.BlockLogicMachine;
 import sunsetsatellite.signalindustries.tiles.machines.TileEntitySIFluidTank;
 import sunsetsatellite.signalindustries.util.Tier;
@@ -50,5 +56,45 @@ public class BlockLogicSIFluidTank extends BlockLogicMachine {
 			return super.getDescription(stack) + "\n" + String.format("Contains: %d/%d mB %s", amount, maxAmount, fluid.getName());
 		}
 		return super.getDescription(stack);
+	}
+
+	@Override
+	public boolean onInteracted(@NotNull World world, @NotNull TilePosc tilePos, @NotNull Player player, @Nullable Side side, double xHit, double yHit) {
+		ItemStack stack = player.getCurrentEquippedItem();
+		TileEntity t = world.getTileEntity(tilePos);
+		if(stack != null && stack.getItem() instanceof IItemFluidContainer item && t instanceof TileEntitySIFluidTank tank){
+			FluidStack fluid = item.getCurrentFluid(stack);
+			if(fluid != null){
+				if (tank.getAllowedFluidsForSlot(0).contains(fluid.fluid)) {
+					if(item.canDrain(stack)){
+						if(tank.getRemainingCapacity(0) <= 0){
+							if (item.canFill(stack)) {
+								ItemStack filled = item.fill(tank.getFluidInSlot(0), stack, tank);
+								if (filled != null) {
+									player.inventory.setCurrentItem(filled);
+								}
+								return true;
+							}
+						} else {
+							item.drain(stack, 0, tank);
+							return true;
+						}
+					} else if (item.canFill(stack)) {
+						ItemStack filled = item.fill(tank.getFluidInSlot(0), stack, tank);
+						if (filled != null) {
+							player.inventory.setCurrentItem(filled);
+						}
+						return true;
+					}
+				}
+			} else if (item.canFill(stack)) {
+				ItemStack filled = item.fill(tank.getFluidInSlot(0), stack, tank);
+				if (filled != null) {
+					player.inventory.setCurrentItem(filled);
+				}
+				return true;
+			}
+		}
+		return super.onInteracted(world, tilePos, player, side, xHit, yHit);
 	}
 }
